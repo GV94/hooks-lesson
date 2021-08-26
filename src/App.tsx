24f8063d3625fb5fdc3372679add427467714cfd
Victor <victorgustafsson@hotmail.com>
2021-08-26 10:12:32 +0200

    add some more reading

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,12 @@ import styled from "styled-components";
     - Make a hook that returns a component that has a button you can press to increase a counter and also displays they current value of the counter
     - Make a hook that makes a request to a given url and returns the data, any errors and a way to make that request again
     - Make a hook that tells you what device the current screen size is for (mobile, tablet or desktop)
+
+  * Read more (its all different sections of the same page of the react docs about hooks)
+    - Introducing hooks: https://reactjs.org/docs/hooks-intro.html, more about why we use hooks
+    - Rules of hooks: https://reactjs.org/docs/hooks-rules.html, more about the rules and why they exist
+    - Hooks at a glance: https://reactjs.org/docs/hooks-overview.html, an overview of hooks
+    - Build your own hooks: https://reactjs.org/docs/hooks-custom.html, more about custom hooks and how to build them
 */
 
 const Container = styled.div``;
